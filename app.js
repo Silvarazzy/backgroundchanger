161d5703ba1f4d23f16636b8f3019c9ccd8dfcc0
Silvarazzy <chukwuemekaogbuka8@gmail.com>
2022-04-27 14:33:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector('.change')
+let bgrcChanger = () =>{
+    let colors = ['red', 'yellow', 'blue', 'green', 'aqua']
+    let random = Math.floor(Math.random()* colors.length)
+    document.body.style.backgroundColor = colors[random]
+}
+
+btn.addEventListener('click', bgrcChanger)
